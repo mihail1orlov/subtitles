44bb03751703a97b7486e2f9d388d0705d44cdfc
Mihail Orlov <mihail_orlov@hotmail.com>
2019-11-11 22:54:44 +0200

    Html prototype is done

--- SubtitlesApp/SubtitlesApp/Program.cs
+++ SubtitlesApp/SubtitlesApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using SubtitlesApp.Model;
 
 namespace SubtitlesApp
@@ -12,6 +13,22 @@ namespace SubtitlesApp
             var items = text.Split("\r\n\r\n");
 
             Subtitle[] subtitles = Parser.Parse(items);
+            var sb = new StringBuilder();
+            foreach (var subtitle in subtitles)
+            {
+                foreach (var content in subtitle.Content)
+                {
+                    sb.Append(content + "</br>");
+                }
+            }
+
+            var title = "terminator 2";
+            var body = sb.ToString();
+            var html = "<!DOCTYPE html><html lang=\"en\"xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta charset=\"utf-8\"/>" +
+                       $"<title>{title}</title>" +
+                       $"</head><body>{body}</body></html>";
+
+            File.WriteAllText($"{path}.html", html);
         }
     }
 }
