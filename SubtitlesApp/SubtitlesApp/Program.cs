3d7eed7675f755d43f74811483b2b14500e0f1cc
Mihail Orlov <mihail_orlov@hotmail.com>
2021-08-26 01:20:24 +0300

    Added Speech functions and speech recognizer

--- SubtitlesApp/SubtitlesApp/Program.cs
+++ SubtitlesApp/SubtitlesApp/Program.cs
@@ -2,8 +2,10 @@
 using Subtitles.Model.Enums;
 using SubtitlesApp.Html;
 using SubtitlesApp.Model;
+using System;
 using System.IO;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -12,27 +14,112 @@ namespace SubtitlesApp
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            CreateDictionary();
-            // CreateSubtitle();
+            //Tupilka();
+            //new TestSpeechRecognizer().Start();
+            Speak();
+            //CreateDictionary();
+            //CreateHtmlFromDictionary();
+            //CreateSubtitle();
+        }
+
+        private static void Tupilka()
+        {
+            using (var tupilka = new Tupilka())
+            {
+                tupilka.Start();
+                do
+                {
+                    Console.Clear();
+                    Console.Write("Enter any line: ");
+                    tupilka.Pause();
+                    var line = Console.ReadLine();
+                    tupilka.UnPause();
+                    Console.Write($"You entered the {line}");
+                } while (Console.ReadKey().Key != ConsoleKey.Q);
+            }
+        }
+
+        private static void Speak()
+        {
+            var devid = new SpeechSynthesizer();
+            var irina = new SpeechSynthesizer();
+            irina.SelectVoice("Microsoft Irina Desktop");
+            devid.SelectVoice("Microsoft David Desktop");
+
+            devid.Speak("look after");
+            irina.Speak("присматривать за");
+            devid.Speak("split up");
+            irina.Speak("разделить");
+            devid.Speak("in common");
+            irina.Speak("в общем");
+            devid.Speak("in contract (with)");
+            irina.Speak("в контракте");
+            devid.Speak("in love (with)");
+            irina.Speak("влюблена");
+            devid.Speak("on purpose");
+            irina.Speak("нарочно");
+            devid.Speak("by yourself");
+            irina.Speak("самостоятельно");
+            devid.Speak("on your own");
+            irina.Speak("самостоятельно");
+
+            Console.ReadKey();
+        }
+
+        private static void CreateHtmlFromDictionary()
+        {
+            GetDictionary(out var options, out var dictionary);
+
+            var body = new StringBuilder();
+            body.Append("<h3><ignore>Unit 12</ignore></h3>");
+            body.Append("<p style=\"font-size: larger;\">");
+            foreach (var item in dictionary)
+            {
+                body.Append($"{item.Text}.</br>");
+            }
+
+            body.Append("</p>");
+            var html = string.Format(HtmlTemplate.Template, "Unit 3", body);
+            File.WriteAllText("D:\\tmp\\slovoSite\\dictionary\\unit_3.html", html);
         }
 
         private static void CreateDictionary()
         {
-            var path = "D:\\repos\\victoria\\mindMaps\\dictionary.json";
-            var source = File.ReadAllText(path);
+            var path = GetDictionary(out var options, out var dictionary);
 
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true,
-                Converters =
-                {
-                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
-                }
-            };
 
-            var dictionary = JsonSerializer.Deserialize<Dictionary>(source, options);
+            dictionary.Add(new Word{Text = "apologise", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb}});
+            dictionary.Add(new Word{Text = "boyfriend", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "close", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "confident", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "cool", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "couple", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "decorate", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb}});
+            dictionary.Add(new Word{Text = "defend", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb}});
+            dictionary.Add(new Word{Text = "divorced", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "flat", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "generous", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "girlfriend", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "grateful", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "guest", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "independent", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "introduce", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "loving", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "loyal", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "mood", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "neighborhood", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "ordinary", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "patient", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "private", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "recognize", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb}});
+            dictionary.Add(new Word{Text = "relation", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "rent", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb, PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "respect", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb, PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "single", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Adjective}});
+            dictionary.Add(new Word{Text = "stranger", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Noun}});
+            dictionary.Add(new Word{Text = "trust", Category = Category.Word, PartsOfSpeech = new [] {PartsOfSpeech.Verb, PartsOfSpeech.Noun}});
 
             dictionary.AddRange(new[]
             {
@@ -49,6 +136,33 @@ namespace SubtitlesApp
 
             var json = JsonSerializer.Serialize(dictionary, options);
             File.WriteAllText(path, json);
+        }
+
+        private static string GetDictionary(out JsonSerializerOptions options, out Dictionary? dictionary)
+        {
+            var path = "D:\\repos\\victoria\\mindMaps\\dictionary_Unit12.json";
+            options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters =
+                {
+                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+                }
+            };
+
+            if (File.Exists(path))
+            {
+                var source = File.ReadAllText(path);
+
+
+                dictionary = JsonSerializer.Deserialize<Dictionary>(source, options);
+            }
+            else
+            {
+                dictionary = new Dictionary();
+            }
+
+            return path;
         }
 
         private static void CreateSubtitle()
---
+++ SubtitlesApp/SubtitlesApp/TestSpeechRecognizer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace SubtitlesApp
+{
+    class TestSpeechRecognizer
+    {
+
+        // Indicate whether the asynchronous emulate recognition  
+        // operation has completed.  
+        static bool completed;
+
+        public void Start()
+        {
+
+            // Initialize an instance of the shared recognizer.  
+            using (SpeechRecognizer recognizer = new SpeechRecognizer())
+            {
+
+                // Create and load a sample grammar.  
+                Grammar testGrammar =
+                  new Grammar(new GrammarBuilder("testing testing"));
+                testGrammar.Name = "Test Grammar";
+                recognizer.LoadGrammar(testGrammar);
+
+                // Attach event handlers for recognition events.  
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(
+                    SpeechRecognizedHandler);
+
+                completed = false;
+                
+                // Wait for the asynchronous operation to complete.  
+                while (!completed)
+                {
+                    Console.WriteLine(recognizer.State);
+                    Thread.Sleep(333);
+                    Console.Clear();
+                }
+
+                completed = false;
+
+                // Start asynchronous emulated recognition.  
+                // This does not match the grammar or generate a SpeechRecognized event.  
+                recognizer.EmulateRecognizeAsync("testing one two three");
+
+                // Wait for the asynchronous operation to complete.  
+                while (!completed)
+                {
+                    Console.WriteLine(recognizer.State);
+                    Thread.Sleep(333);
+                    Console.Clear();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        // Handle the SpeechRecognized event.  
+        static void SpeechRecognizedHandler(
+          object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result != null)
+            {
+                Console.WriteLine("Recognition result = {0}",
+                  e.Result.Text ?? "<no text>");
+                completed = true;
+            }
+            else
+            {
+                Console.WriteLine("No recognition result");
+            }
+        }
+    }
+}
